d8ac878591857b3a3d7f682a7c6a3f69670fb741
Aviral Agarwal <aviralagarwal25@gmail.com>
2021-03-14 18:06:49 +0530

    Make Compatible with testing script
    Remove Strings

--- main.cpp
+++ main.cpp
@@ -9,18 +9,16 @@ using std::endl;
 int main() {
     // Input to be given in format x_left y_top x_right y_bottom
     int n;
-    cout << "Enter The Number of Rectangles" << endl;
     cin >> n;
     coord x_left, y_top, x_right, y_bottom;
     vector<Rectangle> R;
-    cout << "Start Entering the co-ordinates" << endl;
     for (int i = 0; i < n; i++) {
-        cin >> x_left >> y_top >> x_right >> y_bottom;
+        cin >> x_left >> x_right >> y_bottom >> y_top;
         Rectangle rect(x_left, x_right, y_bottom, y_top);
         R.push_back(rect);
     }
     RectangleSet rSet(R);
     rSet.calculateMeasure();
-    cout << "Measure = " << rSet.measure << endl;
+    cout << rSet.measure << endl;
     return 0;
 }
